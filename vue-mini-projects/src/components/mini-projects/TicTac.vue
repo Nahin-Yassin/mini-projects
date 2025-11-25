6b7f3c6398426546980ce904fa9860cf31202792
Nahin-Yassin <135952677+Nahin-Yassin@users.noreply.github.com>
2025-11-25 15:53:40 +0600

    d
    d

--- vue-mini-projects/src/components/mini-projects/TicTac.vue
+++ vue-mini-projects/src/components/mini-projects/TicTac.vue
@@ -1,121 +1,94 @@
-<script>
-import { ref, computed } from 'vue'
+<template>
+  <div class="board">
+    <div class="box box1" @click="handleClick(1)">{{ cells[1] }}</div>
+    <div class="box box2" @click="handleClick(2)">{{ cells[2] }}</div>
+    <div class="box box3" @click="handleClick(3)">{{ cells[3] }}</div>
+
+    <div class="box box4" @click="handleClick(4)">{{ cells[4] }}</div>
+    <div class="box box5" @click="handleClick(5)">{{ cells[5] }}</div>
+    <div class="box box6" @click="handleClick(6)">{{ cells[6] }}</div>
+
+    <div class="box box7" @click="handleClick(7)">{{ cells[7] }}</div>
+    <div class="box box8" @click="handleClick(8)">{{ cells[8] }}</div>
+    <div class="box box9" @click="handleClick(9)">{{ cells[9] }}</div>
+  </div>
+  <button @click="reset">Reset</button>
+</template>
+<script setup>
+import { ref } from 'vue'
+
+const cells = ref({
+  1: '',
+  2: '',
+  3: '',
+  4: '',
+  5: '',
+  6: '',
+  7: '',
+  8: '',
+  9: '',
+})
 
 const player = ref('X')
-const board = ref([
-  ['', '', ''],
-  ['', '', ''],
-  ['', '', ''],
-])
 
-const CalculateWinner = (board) => {
-  const lines = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6],
-  ]
+function handleClick(num) {
+  if (cells.value[num] !== '') return
 
-  for (let i = 0; i < lines.length; i++) {
-    const [a, b, c] = lines[i]
+  cells.value[num] = player.value
 
-    if (board[a] && board[a] === board[b] && board[a] === board[c]) {
-      return board[a]
-    }
-  }
-
-  return null
-}
-
-const winner = computed(() => CalculateWinner(board.value.flat()))
-
-const MakeMove = (x, y) => {
-  if (winner.value) return
-
-  if (board.value[x][y]) return
-
-  board.value[x][y] = player.value
+  checkWinner()
 
   player.value = player.value === 'X' ? 'O' : 'X'
 }
+const reset = () => {
+  cells.value = {
+    1: '',
+    2: '',
+    3: '',
+    4: '',
+    5: '',
+    6: '',
+    7: '',
+    8: '',
+    9: '',
+  }
+}
+function checkWinner() {
+  const c = cells.value
 
-const ResetGame = () => {
-  board.value = [
-    ['', '', ''],
-    ['', '', ''],
-    ['', '', ''],
-  ]
-  player.value = 'X'
+  if (c[1] && c[1] === c[2] && c[2] === c[3]) {
+    alert(`${c[1]} wins`)
+  } else if (c[4] && c[4] === c[5] && c[5] === c[6]) {
+    alert(`${c[4]} wins`)
+  } else if (c[7] && c[7] === c[8] && c[8] === c[9]) {
+    alert(`${c[7]} wins`)
+  } else if (c[1] && c[1] === c[4] && c[4] === c[7]) {
+    alert(`${c[1]} wins`)
+  } else if (c[2] && c[2] === c[5] && c[5] === c[8]) {
+    alert(`${c[2]} wins`)
+  } else if (c[3] && c[3] === c[6] && c[6] === c[9]) {
+    alert(`${c[3]} wins`)
+  } else if (c[1] && c[1] === c[5] && c[5] === c[9]) {
+    alert(`${c[1]} wins`)
+  } else if (c[3] && c[3] === c[5] && c[5] === c[7]) {
+    alert(`${c[3]} wins`)
+  }
 }
 </script>
-<template>
-  <div id="title">
-    <h1>Tic Tac Toe</h1>
-  </div>
-  <div id="board">
-    <div class="square" id="square0"></div>
-    <div class="square" id="square1"></div>
-    <div class="square" id="square2"></div>
-    <div class="square" id="square3"></div>
-    <div class="square" id="square4"></div>
-    <div class="square" id="square5"></div>
-    <div class="square" id="square6"></div>
-    <div class="square" id="square7"></div>
-    <div class="square" id="square8"></div>
-  </div>
-
-  <div id="board">{{ board }}</div>
-  <div id="endGame">
-    <button class="reset" @click="ResetGame">Reset</button>
-  </div>
-</template>
-<style>
-h1 {
-  text-align: center;
+<style scoped>
+.board {
+  display: grid;
+  grid-template-columns: repeat(3, 100px);
+  gap: 5px;
 }
-
-#board {
-  margin-left: auto;
-  margin-right: auto;
-  width: 375px;
-  height: 375px;
-  display: flex;
-  flex-wrap: wrap;
-}
-
-.square {
-  width: 120px;
-  height: 120px;
-  border: 1px solid #d3d3d3;
-  background-color: #f5f5f5;
-  font-size: 40px;
+.box {
+  width: 100px;
+  height: 100px;
+  background: #ddd;
   display: flex;
   justify-content: center;
   align-items: center;
-}
-
-.square:hover {
-  background-color: #ffffe0;
-}
-
-.reset {
-  display: block;
-  margin-left: auto;
-  margin-right: auto;
-  height: 40px;
-  width: 150px;
-  background-color: #ffffff;
-  border: 1px solid #000000;
-  border-radius: 40px;
-  font-size: 18px;
-}
-
-.reset:hover {
-  background-color: #000000;
-  color: #ffffff;
+  font-size: 40px;
+  cursor: pointer;
 }
 </style>
