d2899fc655f90d4dce2ca7c787bad17e46ab2ee0
Nahin-Yassin <135952677+Nahin-Yassin@users.noreply.github.com>
2025-12-30 14:37:22 +0600

    s
    s

--- vue-mini-projects/src/components/mini-projects/ReactionTimer.vue
+++ vue-mini-projects/src/components/mini-projects/ReactionTimer.vue
@@ -1,78 +1,52 @@
 <script setup>
 import { ref } from 'vue'
+let waiting = ref(0)
 
-const isGreen = ref(false) // green light
-const isWaiting = ref(false) // "wait..." state
-let startTime = ref(0)
-
-function startGame() {
-  isWaiting.value = true // start with "wait..." state
-  const delay = Math.floor(Math.random() * 3000) + 2000
-
+function waitingTime(delay = 5500) {
+  waiting = Math.floor(Math.random() * delay) + 3000
+  console.log(waiting)
   setTimeout(() => {
-    isGreen.value = true
-    isWaiting.value = false
-    startTime.value = performance.now()
+    console.log(`Delayed for ${waiting} ms`)
   }, delay)
 }
-
-function start() {
-  startGame()
-}
+waitingTime()
 </script>
-
 <template>
   <div class="container">
-    <div v-if="!isWaiting && !isGreen" class="before" @click="start">
-      <p>
-        Reaction time tester <br />
-        click anywhere to start
-      </p>
-    </div>
-    <div v-else-if="isWaiting" class="after">
-      <p>wait...</p>
-    </div>
-    <div v-else class="red_room">
-      <p>Click now!</p>
+    <p>
+      Reaction time test <br />
+      click below to start
+    </p>
+    <div class="game_box" v-if="waitingTime" @click="waitingTime">
+      <div class="for_green">
+        <p>wait for green</p>
+      </div>
     </div>
   </div>
 </template>
 
-<style scoped>
+<style>
 .container {
-  display: flex;
-  justify-content: center;
-  width: 1280px;
+  width: 500px;
   height: 500px;
-}
-
-.before {
   display: flex;
   justify-content: center;
   align-items: center;
-  font-size: 40px;
-  width: 100%;
-  height: 100%;
-  background-color: transparent;
+  flex-direction: column;
+  border: solid var(--clr-accent);
 }
-
-.after {
+.game_box {
   display: flex;
   justify-content: center;
   align-items: center;
-  font-size: 40px;
-  width: 100%;
-  height: 100%;
-  background-color: var(--clr-accent);
-}
-.red_room {
-  cursor: pointer;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  font-size: 40px;
-  width: 100%;
-  height: 100%;
   background-color: red;
+  width: 350px;
+  height: 350px;
+}
+
+.for_green {
+  background-color: green;
+  width: 300px;
+  height: 300px;
 }
 </style>
